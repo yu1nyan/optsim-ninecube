e6be319c235959788c1e564c794f94d0ead411e4
awayui <yuiyuiawayui@gmail.com>
2019-10-29 18:31:51 +0900

    Fix a script

--- run100-100.sh
+++ run100-100.sh
@@ -3,5 +3,5 @@ echo $nowdir
 for((i=0;i<100;i++))
 do
  #echo $i
- bsub -q s -J "$nowdir-$i" "../build/wls ./100evt.mac ./$nowdir-$i.root"
+ bsub -q s -J "$nowdir-$i" "../build/wls ./100evt.mac ./$nowdir-$i.root $i"
 done
